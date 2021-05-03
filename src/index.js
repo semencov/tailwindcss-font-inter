a7944f371399d30d826194e17abcca772b59bef8
Yuri Sementsov <hello@smcv.dev>
2021-05-03 17:21:06 +0300

    chore: minor fixes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ const Inter = require('../inter.json')
 
 const isBoolean = val => typeof val === 'boolean'
 const isString = val => typeof val === 'string'
-const isArrayLike = obj => obj != null && !isString(obj) && typeof obj[Symbol.iterator] === 'function'
+const isArrayLike = obj =>
+  obj && obj !== null && !isString(obj) && typeof obj[Symbol.iterator] === 'function'
 const isPlainObject = val => !!val && typeof val === 'object' && val.constructor === Object
-const mapObject = (obj, cb) => Object.fromEntries((Array.isArray(obj) ? obj : Object.entries(obj)).map(val => cb(...val)))
-const filterObject = (obj, cb) => Object.fromEntries((Array.isArray(obj) ? obj : Object.entries(obj)).filter(val => cb(...val)))
+const mapObject = (obj, cb) =>
+  Object.fromEntries((Array.isArray(obj) ? obj : Object.entries(obj)).map(val => cb(...val)))
+const filterObject = (obj, cb) =>
+  Object.fromEntries((Array.isArray(obj) ? obj : Object.entries(obj)).filter(val => cb(...val)))
 const defaults = (obj, ...defs) => Object.assign({}, obj, ...defs.reverse(), obj)
 const round = (num, prec = 3) => parseFloat(num.toFixed(prec))
 const unquote = str => str.replace(/^['"]|['"]$/g, '').trim()
@@ -22,14 +25,10 @@ const normalizeEntry = (key, value) => {
 
 const generateFeatures = (features, available) => {
   if (isPlainObject(features)) {
-    features = mapObject(features, (key, value = '1') =>
-      normalizeEntry(key, value)
-    )
+    features = mapObject(features, (key, value = '1') => normalizeEntry(key, value))
   } else {
     if (isString(features)) {
-      features = Object.fromEntries(
-        features.split(',').map(f => f.trim().split(' '))
-      )
+      features = Object.fromEntries(features.split(',').map(f => f.trim().split(' ')))
     }
 
     features = Object.fromEntries(
@@ -59,7 +58,7 @@ const generateFeatures = (features, available) => {
     .trim()
 }
 
-module.exports = function (options = {}) {
+module.exports = function(options = {}) {
   return ({ addBase, addUtilities, variants, e, theme }) => {
     const { availableFeatures, utilities, base } = Inter
 
@@ -68,7 +67,7 @@ module.exports = function (options = {}) {
       b: 0.185,
       c: -0.1745,
       baseFontSize: 16,
-      importFontFace: true
+      importFontFace: true,
     })
 
     const defaultFontFeaturesTheme = { default: ['calt', 'kern'] }
@@ -80,14 +79,14 @@ module.exports = function (options = {}) {
     const fontFeaturesTheme = theme('interFontFeatures', defaultFontFeaturesTheme)
     const fontFeatures = defaults(fontFeaturesTheme, defaultFontFeaturesTheme)
     const baseStyles = {
-      ...(defaultConfig.importFontFace ? base : {})
+      ...(defaultConfig.importFontFace ? base : {}),
     }
 
     const fontSizeStyles = (fontSize, { a, b, c }, letterSpacing = 0) => {
       const [size, opts = {}] = isArrayLike(fontSize) ? fontSize : [fontSize]
       const sizeInPx = toPx(size, defaultConfig.baseFontSize)
       const baseTracking = toPx(letterSpacing, sizeInPx)
-      const trackingSize = (baseTracking / sizeInPx) + tracking(sizeInPx, a, b, c)
+      const trackingSize = baseTracking / sizeInPx + tracking(sizeInPx, a, b, c)
 
       return {
         ...opts,
@@ -99,7 +98,7 @@ module.exports = function (options = {}) {
     const fontFeatureStyles = value => {
       return value.length
         ? {
-            fontFeatureSettings: Array.isArray(value) ? value.join(', ') : value
+            fontFeatureSettings: Array.isArray(value) ? value.join(', ') : value,
           }
         : null
     }
@@ -107,47 +106,52 @@ module.exports = function (options = {}) {
     const fontFeatureUtilities = {
       ...{
         '.font-inter .font-feature-normal, .font-inter.font-feature-normal': {
-          ...fontFeatureStyles('normal')
-        }
+          ...fontFeatureStyles('normal'),
+        },
       },
       ...mapObject(fontFeatures, (modifier, value) => {
         const features = generateFeatures(value, availableFeatures)
 
         return [
-          `.font-inter .${e(`font-feature-${modifier}`)},.font-inter.${e(`font-feature-${modifier}`)}`,
+          `.font-inter .${e(`font-feature-${modifier}`)},.font-inter.${e(
+            `font-feature-${modifier}`
+          )}`,
           {
-            ...fontFeatureStyles(features)
-          }
+            ...fontFeatureStyles(features),
+          },
         ]
-      })
+      }),
     }
 
-    const fontSizeUtilities = Object.entries(fontSizeTheme).reduce((result, [sizeModifier, sizeValue]) => {
-      const { a, b, c } = defaultConfig
+    const fontSizeUtilities = Object.entries(fontSizeTheme).reduce(
+      (result, [sizeModifier, sizeValue]) => {
+        const { a, b, c } = defaultConfig
 
-      return {
-        ...result,
-        ...Object.fromEntries([
-          [
-            `.font-inter .${e(`text-${sizeModifier}`)}, .font-inter.${e(`text-${sizeModifier}`)}`,
-            {
-              ...fontSizeStyles(sizeValue, { a, b, c })
-            }
-          ],
-          ...Object.entries(letterSpacingTheme).map(([spacingModifier, spacingValue]) => [
-            `.font-inter .${e(`text-${sizeModifier}`)} .${e(`tracking-${spacingModifier}`)}, ` +
-            `.font-inter .${e(`text-${sizeModifier}`)}.${e(`tracking-${spacingModifier}`)}, ` +
-            `.font-inter.${e(`text-${sizeModifier}`)} .${e(`tracking-${spacingModifier}`)}, ` +
-            `.font-inter.${e(`text-${sizeModifier}`)}.${e(`tracking-${spacingModifier}`)}`,
-            {
-              ...fontSizeStyles(sizeValue, { a, b, c }, spacingValue)
-            }
-          ])
-        ])
-      }
-    }, {})
-
-    console.log(fontSizeUtilities);
+        return {
+          ...result,
+          ...Object.fromEntries([
+            [
+              `.font-inter .${e(`text-${sizeModifier}`)}, .font-inter.${e(`text-${sizeModifier}`)}`,
+              {
+                ...fontSizeStyles(sizeValue, { a, b, c }),
+              },
+            ],
+            ...Object.entries(letterSpacingTheme).map(([spacingModifier, spacingValue]) => [
+              `.font-inter .${e(`text-${sizeModifier}`)} .${e(`tracking-${spacingModifier}`)}, ` +
+                `.font-inter .${e(`text-${sizeModifier}`)}.${e(`tracking-${spacingModifier}`)}, ` +
+                `.font-inter.${e(`text-${sizeModifier}`)} .${e(`tracking-${spacingModifier}`)}, ` +
+                `.font-inter.${e(`text-${sizeModifier}`)}.${e(`tracking-${spacingModifier}`)}, ` +
+                `.${e(`tracking-${spacingModifier}`)} .font-inter .${e(`text-${sizeModifier}`)}, ` +
+                `.${e(`tracking-${spacingModifier}`)} .font-inter.${e(`text-${sizeModifier}`)}`,
+              {
+                ...fontSizeStyles(sizeValue, { a, b, c }, spacingValue),
+              },
+            ]),
+          ]),
+        }
+      },
+      {}
+    )
 
     // Add @font-face if importFontFace: true
     // see https://rsms.me/inter/inter.css
